0d044ecf78c7d09dc5491ee6b26e9a3c04fec402
katie-gu <katiegu555@gmail.com>
2018-07-08 19:18:24 -0700

    Working on homepage.

--- mysite/game/views.py
+++ mysite/game/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. more text")
+def home(request):
+    return render(request, 'game/home.html')

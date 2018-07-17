94710692e1811a379bcc53d1b0d78d890a99558c
katie-gu <katiegu555@gmail.com>
2018-07-17 09:34:26 -0700

    Added challenges to database.

--- mysite/game/views.py
+++ mysite/game/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
+from game.models import Challenge
 
 def home(request):
     return render(request, 'game/home.html')
 
 def profile(request):
-    #query(data)    
-    return render(request, "game/profile.html")
+    Challenge.objects.create(description="Make a new friend.", xp=50)
+    Challenge.objects.create(description="Go for a walk.", xp=100)
+    challenge1 = Challenge.objects.get(description="Make a new friend.", xp=50)
+    challenge2 = Challenge.objects.get(description="Go for a walk.", xp=100)
+    return render(request, "game/profile.html", {'challenge1': challenge1, 'challenge2' : challenge2})
